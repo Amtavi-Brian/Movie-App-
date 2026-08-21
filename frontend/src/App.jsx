8113fdac84ce638a10a09cbfc45d44baddb7137c
Amtavi-Brian <brian.amtavi@strathmore.edu>
2026-08-21 13:17:30 +0300

    navbar addition

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 
 import Home from './pages/Home';
+import Favourite from './pages/Favourite';
+import {Routes, Route} from 'react-router-dom';
+import NavBar from './assets/Components/NavBar';
 function App(){
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourite" element={<Favourite />} />
+       </Routes>
+      </main>
+    </div>
   );
 }
 export default App;
---
+++ frontend/src/pages/Favourite.jsx
@@ -0,0 +1,9 @@
+function Favourite() {
+    return(
+        <div className="favourite-empty">
+            <h2>No favourite Movies Yet</h2>
+            <p>Start adding movies to your favourites and they will apper here</p>
+        </div>
+    );
+}
+export default Favourite;
